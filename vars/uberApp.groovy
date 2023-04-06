a860b1711dbc9154d34f9cd41ce5d0eec167ec02
pretei45 <pretei4@gmail.com>
2023-04-06 17:49:21 -0500

    new app added

---
+++ vars/uberApp.groovy
@@ -0,0 +1,33 @@
+def call(String repoUrl){
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage('demo-newapp'){
+            steps{
+                sh 'cat /etc/passwd'
+                echo "jenkins was an easy module"
+            }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
+}
